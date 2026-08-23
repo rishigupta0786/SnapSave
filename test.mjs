7e2738e45f3639f44b37a733ad6dc9cbacca548a
Rishi gupta <rg00663@gmail.com>
2026-08-23 23:26:40 +0530

    chore: remove unwanted scratch and test files

--- test.mjs
+++
@@ -1,3 +0,0 @@
-import { snapsave } from 'snapsave-media-downloader';
-
-snapsave('https://www.instagram.com/p/C_KjV9-P1Q5/').then(console.log).catch(console.error);
--- test_api.js
+++
@@ -1,9 +0,0 @@
-const url = "http://localhost:3000/api/analyze";
-fetch(url, {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ url: "https://www.youtube.com/watch?v=zBjJUV-lzHo" })
-})
-.then(res => res.json())
-.then(console.log)
-.catch(console.error);
